052b9b8499babf450c38cf4bdc018bce780474de
Josué López <josuloop@icloud.com>
2021-11-13 14:59:48 -0600

    Feat: Contador de divisores
    Se agregó un contador y una impresión del número total de divisores.

--- divisor.cpp
+++ divisor.cpp
@@ -2,19 +2,22 @@
 
 int main ()
 {
-	int num, x;
+	int num, x, contador=0;
 	
 	do{
-		printf("Introduce un número entero: ");
+		printf("Introduce un numero entero: ");
 		scanf("%d", &num);
 	}while(num<=0);
 	
-	for(x=1;x<=num;x++)
+	for(x=1; x<=num; x++)
 	{
 		if(num%x==0)   
 		{
 			printf("Su divisor es: %d \n",x);
+			contador ++; 
+			// incrementas el contador cada vez que haya un divisor
 		}
 	}
+	printf("\n El nÃºmero %d tiene un total de: %d divisores. \n",num ,contador);
 	return 0;
 }
